145eb81a570f67a981ebf3a874a3498127c38a00
Jhon Rey <jhonreymendiola@gmail.com>
2023-07-08 19:11:46 +0800

    scroll animate

--- src/app/modules/main/home/home.component.ts
+++ src/app/modules/main/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {  Component, HostListener, ElementRef  } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  activeSection: HTMLElement | null = null;
+
+  constructor(private elementRef: ElementRef) {}
+
+  @HostListener('window:scroll')
+  onScroll(): void {
+    const sections = this.elementRef.nativeElement.querySelectorAll('.sec');
+    const scrollPosition = window.scrollY;
+
+    for (let i = 0; i < sections.length; i++) {
+      const section = sections[i] as HTMLElement;
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.offsetHeight;
+
+      if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
+        this.activeSection = section;
+        break;
+      }
+    }
+  }
 
 }
