0239e4cdaf62790f4d0abe00a45cecf39cbfce35
Jhon Rey <jhonreymendiola@gmail.com>
2023-08-02 21:28:24 +0800

    working contact

--- src/app/modules/main/home/home.component.ts
+++ src/app/modules/main/home/home.component.ts
@@ -1,6 +1,13 @@
 import {  Component, HostListener, ElementRef  } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import emailjs from '@emailjs/browser';
+
+
+// Custom email validator function
+function isValidEmail(control: FormControl) {
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i;
+  return emailPattern.test(control.value) ? null : { invalidEmail: true };
+}
 
 @Component({
   selector: 'app-home',
@@ -35,9 +42,22 @@ export class HomeComponent {
     }
   }
   async send(){
+    if(this.form.value.from_name.length <= 0 || 
+      this.form.value.from_email.length <= 0 || 
+      this.form.value.subject.length <= 0 || 
+      this.form.value.message.length <= 0){
+      return alert('All Fields are required')
+
+    }
+    
+    const fromEmailFormControl = this.form.get('from_email') as FormControl;
+    if (fromEmailFormControl.invalid || !fromEmailFormControl.value.includes('@')) {
+      return alert('Invalid email');
+    }
+
     emailjs.init('H5dz_L9_DPJUVZf7c')
     let response = await emailjs.send("service_p54fhys","template_59h2538",{
-      from_name: this.form.value.form_name,
+      from_name: this.form.value.from_name,
       to_name:  this.form.value.to_name,
       from_email:  this.form.value.from_email,
       subject:  this.form.value.subject,
